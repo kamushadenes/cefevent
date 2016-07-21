171aa689bf6b33a1bc84a91ec64f36609a850c0d
Kamus Hadenes <kamushadenes@hyadesinc.com>
2016-07-21 03:42:54 -0300

    Replace README.md with README.rst

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+import os
 from distutils.core import setup
+
+long_description = 'ArcSight\'s Common Event Format library',
+if os.path.exists('README.txt'):
+    long_description = open('README.txt', 'r').read()
+
+
 setup(
   name = 'cefevent',
   packages = ['cefevent'], # this must be the same as the name above
   version = '0.1',
   description = 'ArcSight\'s Common Event Format library',
+  long_description = long_description,
   author = 'Kamus Hadenes',
   author_email = 'kamushadenes@hyadesinc.com',
   url = 'https://github.com/kamushadenes/cefevent', # use the URL to the github repo
